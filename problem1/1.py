aba53758154498b163f9ad0e4c987ec0767290ef
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2019-12-07 13:53:21 +0000

    problem 1 solved

--- problem1/1.py
+++ problem1/1.py
@@ -1,8 +1,21 @@
 from functools import reduce
 
 def fuel_requirements(mass):
-    return (mass // 3) - 2
-    
-    
-input = open('input').read().splitlines()    
-fuel_counter = reduce(lambda x,y:x + fuel_requirements(y), input)
+    fuel = (int(mass) // 3) - 2
+    result = 0
+    while fuel > 0:
+        result += fuel
+        fuel = (int(fuel) // 3) - 2
+    return result
+
+
+input = open('input').read().splitlines()
+
+
+fuel_counter = [fuel_requirements(mass) for mass in input]
+
+total_fuel_required = reduce(lambda x, y: x + y ,fuel_counter)
+
+print("Total Fuel required is: " + str(total_fuel_required))
+
+
